943c78540a1a6216321c5d49cf14f5ff2523ee39
arun0180 <biradararun333@gmail.com>
2026-05-21 09:49:52 +0530

    new update

--- client/vite.config.js
+++ client/vite.config.js
@@ -3,20 +3,23 @@ import tailwindcss from "@tailwindcss/vite"
 import react from "@vitejs/plugin-react"
 import { defineConfig } from "vite"
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
 
-   server: {
+  server: {
     port: 5173,
+    host: true,
+    allowedHosts: true,
+
     proxy: {
       "/api": {
-        target: "http://localhost:5000", 
+        target: "http://backend:5000",
         changeOrigin: true,
         secure: false,
       },
